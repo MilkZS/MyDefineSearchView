316609e59f878cc7afda518cba5cbddd8ce53697
zuoqi <774765813@qq.com>
2018-07-16 21:49:09 +0800

    search tag

--- app/src/main/java/com/milkzs/android/mydefinesearchview/MainActivity.java
+++ app/src/main/java/com/milkzs/android/mydefinesearchview/MainActivity.java
@@ -2,6 +2,9 @@ package com.milkzs.android.mydefinesearchview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.flexbox.FlexboxLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.search_tag);
+        FlexboxLayout flexboxLayout = findViewById(R.id.float_layout_search_tag);
+        for(int i=0;i<12;i++){
+            TextView textView = new TextView(this);
+            textView.setText("阿萨法发");
+            flexboxLayout.addView(textView);
+        }
     }
 }
